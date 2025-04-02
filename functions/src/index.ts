0b7e4ccdf9d16000edc9ae6ddbe7fdf47340401b
steveoh <sgourley@utah.gov>
2025-04-02 17:14:10 -0600

    feat(api): create a blocking function to only allow specific users access

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,10 @@
+import { initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import { beforeUserSignedIn, HttpsError, type AuthBlockingEvent } from 'firebase-functions/identity';
+import { logger } from 'firebase-functions/v2';
 import { onRequest } from 'firebase-functions/v2/https';
 
-const cors = [/ut-dts-ugrc-plss-dev\.web\.app$/, /localhost:\d+$/];
+const cors = [/ut-dts-agrc-plss-dev-staff-review\.web\.app$/, /localhost:\d+$/];
 
 const health = onRequest({ cors, region: 'us-west3' }, async (_, res) => {
   res.send('healthy');
@@ -8,3 +12,31 @@ const health = onRequest({ cors, region: 'us-west3' }, async (_, res) => {
 
 // Only export health check in emulator mode
 export const healthCheck = process.env.FUNCTIONS_EMULATOR === 'true' ? health : undefined;
+
+initializeApp();
+
+const db = getFirestore();
+
+async function checkUser(event: AuthBlockingEvent) {
+  const email = event.data?.email;
+  logger.debug('checkUser', { email });
+
+  if (!email) {
+    throw new HttpsError('invalid-argument', 'Email is required');
+  }
+
+  const ref = db.collection('authorized-users').doc(email.trim().toLowerCase());
+  const doc = await ref.get();
+
+  if (!doc.exists) {
+    throw new HttpsError('permission-denied', `User (${email}) is not authorized`);
+  }
+}
+
+// The PLSS submission app already has a blocking function to create profiles.
+// export const beforeCreated = beforeUserCreated(checkUser);
+
+/* This may not be necessary since beforeUserCreated fires before beforeUserSignedIn. Leaving it here in case it's needed for existing users.
+ This also would be a good place to add additional checks for existing users.
+ */
+export const beforeSignedIn = beforeUserSignedIn(checkUser);
