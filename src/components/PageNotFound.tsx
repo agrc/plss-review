fa66d1d6064b7fb3040dd970efb95cff83a498f9
steveoh <sgourley@utah.gov>
2025-04-17 18:31:05 -0600

    refactor: rename error for clarity

---
+++ src/components/PageNotFound.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@ugrc/utah-design-system';
+import { useNavigate } from 'react-router';
+
+export function PageNotFound() {
+  const message = '404';
+  const details = 'This page does not exist';
+  let stack: string | undefined;
+  const navigate = useNavigate();
+
+  return (
+    <main className="mx-auto flex flex-1 flex-col items-center gap-3 pt-16">
+      <h1>{message}</h1>
+      <p>{details}</p>
+      {stack && (
+        <pre className="w-full overflow-x-auto p-4">
+          <code>{stack}</code>
+        </pre>
+      )}
+      <Button size="extraLarge" onPress={() => navigate(-1)}>
+        back
+      </Button>
+    </main>
+  );
+}
