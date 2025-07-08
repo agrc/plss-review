dc5c9802ed7f61b007752cc41600d214eae4b237
steveoh <sgourley@utah.gov>
2025-07-08 16:35:20 -0600

    test: refactor tests and correct time duration

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import loadVersion from 'vite-plugin-package-version';
@@ -8,5 +9,15 @@ export default defineConfig({
   resolve: {
     // this is only applicable when pnpm-linking the utah-design-package
     dedupe: ['firebase', '@arcgis/core'],
+  },
+  test: {
+    environment: 'node',
+    globals: true,
+    env: {
+      FIRESTORE_EMULATOR_HOST: '127.0.0.1:8080',
+      FIREBASE_AUTH_EMULATOR_HOST: '127.0.0.1:9099',
+      FIREBASE_STORAGE_EMULATOR_HOST: '127.0.0.1:9199',
+      GCLOUD_PROJECT: 'test-project',
+    },
   },
 });
