3dcf3f57de9d84e57cb5297cf368d6ec9f324776
Alex Neville <acneville@utah.gov>
2026-07-01 14:13:00 -0600

    chore: abstracting out rejection reasons

---
+++ functions/shared/types.ts
@@ -0,0 +1,8 @@
+export const rejectionReasonLabels = {
+  'missing-photo': 'Irrelevant or missing photos',
+  'incomplete-location': 'Inaccurate or incomplete location information',
+  'illegible-scan': 'Illegible or poorly scanned document',
+  'incomplete-description': 'Incorrect monument description',
+  'incomplete-sheet': 'Missing required fields',
+  other: 'Other',
+} as const;
