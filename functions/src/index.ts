212349aaa94a41f3aedfdf1fa15c7a8eab4c2279
Alex Neville <acneville@utah.gov>
2026-07-02 14:23:34 -0600

    feat: refactoring to secret paradigm

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,6 +9,7 @@ import { approveCounty, authorizeUser, publishSubmissions, queueTasks, sendMail 
 const sendGridApiKey = defineSecret('SENDGRID_API_KEY');
 const agolCredentials = defineSecret('AGOL_CREDENTIALS');
 const testRecipientEmail = defineSecret('TEST_RECIPIENT_EMAIL');
+const sendGridTemplateId = defineSecret('SENDGRID_TEMPLATE_ID');
 
 const basicQueueSettings = {
   retryConfig: {
@@ -35,7 +36,7 @@ export const autoApprovals = onTaskDispatched({ ...basicQueueSettings }, approve
 export const email = onTaskDispatched(
   {
     ...basicQueueSettings,
-    secrets: [sendGridApiKey, testRecipientEmail],
+    secrets: [sendGridApiKey, testRecipientEmail, sendGridTemplateId],
   },
   sendMail,
 );
